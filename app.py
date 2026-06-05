b0a3ed84d4a9c9595bd2f0eef3d2ed1731ce31b1
GimyMh <kimmh0757@gmail.com>
2026-06-05 17:18:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -117,6 +117,10 @@ def calculate_carbon_footprint(disposable_items_car_usage_counts, used_public_tr
 def run_calculator():
     st.title("🌱 탄소 발자국 계산기")
 
+    disposable_items_car_usage_counts = get_disposable_item_car_usage_input()
+    used_public_transport = get_public_transport_input()
+    meat_counts = get_meat_consumption_input()
+    recycled = get_recycling_input()
 
     if st.button("탄소 발자국 계산"):
 
