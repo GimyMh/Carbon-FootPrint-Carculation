81c67f86b2af2d23f6ea27a5a8840a722adace9f
GimyMh <kimmh0757@gmail.com>
2026-06-05 14:25:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ def plot_carbon_footprint(carbon_score, feedback_message):
     xy=(carbon_score, 0),
     xytext=(carbon_score, 0.4),
     ha="center",
-    arrowprops=dict(arrowstyle="-->")
+    arrowprops=dict(arrowstyle="->")
 )
 
 
